b4f55ecc9fcebddc1c2a3a2d833309e94e78a29d
Lasse <lasse96-06@hotmail.com>
2018-10-23 11:49:04 +0200

    kjhg

--- src/HttpCoordinateManager.java
+++ src/HttpCoordinateManager.java
@@ -1,12 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import org.json.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -18,10 +9,22 @@ import org.xml.sax.SAXParseException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class HttpCoordinateManager
 {
     private static HttpCoordinateManager ourInstance = new HttpCoordinateManager();
+    public static ArrayList<Coordinate> coordinates = new ArrayList<>();
+    public static int errorCount = 0;
+    public static int danishNodes = 0;
 
     private HttpCoordinateManager()
     {
@@ -37,16 +40,14 @@ public class HttpCoordinateManager
         return ourInstance;
     }
 
-    public static void testHttpCall()
+    public static void cacheCoordinatesFromId(long nodeId)
     {
+        String stringNodeId = nodeId + "";
         URL url = null;
         try
         {
             StringBuilder sBuilder = new StringBuilder();
-            sBuilder.append("https://www.openstreetmap.org/api/0.6/node/2232529792");
-
-            // First parameter
-            //sBuilder.append("id_token=");
+            sBuilder.append("https://www.openstreetmap.org/api/0.6/node/" + stringNodeId);
 
             String requestUrl = sBuilder.toString();
             url = new URL(requestUrl);
@@ -58,7 +59,8 @@ public class HttpCoordinateManager
             int status = con.getResponseCode();
             if (status != 200)
             {
-                System.out.println("Der skete en fejl");
+                errorCount++;
+                return;
                 //TODO: Log an error
 
             }
@@ -100,28 +102,36 @@ public class HttpCoordinateManager
             });
 
 
-
             Document doc = builder.parse(new InputSource(new StringReader(content.toString())));
             doc.normalize();
 
             NodeList list = doc.getElementsByTagName("node");
-            for(int i = 0; i < list.getLength(); i++)
+            for (int i = 0; i < list.getLength(); i++)
             {
                 Node node = list.item(i);
                 String lat = node.getAttributes().getNamedItem("lat").getNodeValue();
                 String lon = node.getAttributes().getNamedItem("lon").getNodeValue();
-                System.out.println(lat);
-                System.out.println(lon);
+                if (lat != null && lon != null)
+                {
+                    coordinates.add(new Coordinate()
+                    {{
+                        setLatitude(lat);
+                        setLongitude(lon);
+                        setNodeId(nodeId);
+                        if(DenmarkChecker.isThisDenmark(this))
+                        {
+                            setDenmark(true);
+                            danishNodes++;
+                        }
+                    }});
+                }
+
 
             }
 
-            // Convert content from BufferedReader into a JSON-Object.
-//            JSONTokener tokener = new JSONTokener(content.toString());
-//            JSONObject json = new JSONObject(tokener);
-//            String aud = json.getString("aud");
 
 
-            //TODO: Log an error
+
 
 
         }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,51 @@
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws ExecutionException, InterruptedException
     {
-        HttpCoordinateManager.getInstance().testHttpCall();
+        long startId = 2232529700L;
+        long endId = startId + 100;
+        AtomicInteger arbejde = new AtomicInteger(1);
+        Collection<Future<?>> futures = new LinkedList<Future<?>>();
+
+        System.out.println("Begynder " + (endId - startId) + " HTTP kald...");
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        for (long i = startId; i < endId; i++)
+        {
+            long finalI = i;
+            futures.add(executor.submit(() ->
+            {
+                HttpCoordinateManager.getInstance().cacheCoordinatesFromId(finalI);
+                System.out.println(arbejde.getAndIncrement() + " kald lavet...");
+            }));
+
+
+        }
+
+        for (Future<?> future : futures)
+        {
+            future.get();
+        }
+
+        for (Coordinate c : HttpCoordinateManager.getInstance().coordinates)
+        {
+            System.out.println("-------------------------------------------");
+            System.out.println("Lat: " + c.getLatitude());
+            System.out.println("Lon: " + c.getLongitude());
+            System.out.println("Denmark: " + c.isDenmark());
+        }
+        System.out.println("-------------------------------------------\"");
+        System.out.println("Antal fejl: " + HttpCoordinateManager.getInstance().errorCount);
+        System.out.println("Antal danske noder: " + HttpCoordinateManager.getInstance().danishNodes);
+
+        executor.shutdown();
     }
 }
